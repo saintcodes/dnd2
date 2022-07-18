a018921a2251a7b3bcea5f5371621041c4093bc7
Michelle <clairsin@gmail.com>
2022-07-18 17:15:03 -0400

    updated logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-
-  function handleClick() {
-    setCount(count => count +1)
-  }
 
   return (
-    <div className="App">
-      
+    <div className="screen">
+      {/* adds video of fire in the background */}
+      <video autoPlay muted loop id="myVideo">
+       <source src="https://a2development.s3.amazonaws.com/H264HD1080.mov" type="video/mp4" />
+      </video>
+      <div className="" id="fs-overlay"></div>
+      <img 
+        id="header-img"
+        alt="dungeons and dragons logo"
+        src="https://1000logos.net/wp-content/uploads/2021/08/Dungeons-and-Dragons-Logo-2014.png"
+      />
     </div>
   );
 }
